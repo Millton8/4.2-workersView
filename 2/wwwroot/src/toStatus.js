8f7ae24ea5fa5b57ebe56a8d589d5215bf606a0a
Alexandr <128663508+Millton8@users.noreply.github.com>
2023-10-15 13:08:45 +0700

    Delete 2 directory

--- 2/wwwroot/src/toStatus.js
+++
@@ -1,8 +0,0 @@
-﻿
-const bt = document.getElementById("toStatus")
-bt.addEventListener("click", async e => {
-    e.preventDefault();
-    console.log("ddddd")
-    window.location.replace(`/status.html`)
-}
-)
